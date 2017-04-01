fcf1f6f291bcb63010d31652b5b6b5b21ae4422b
lyzgg5 <lyzgg5@gmail.com>
2017-04-02 00:03:30 +0800

    限黌群組的創建人或成員才能編輯更新刪除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -13,7 +14,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def create
@@ -28,7 +28,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
       if @group.update(group_params)
         redirect_to groups_path, notice: "Update Success更新成功"
       else
@@ -37,13 +36,19 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
-
     redirect_to groups_path, alert: "Group Deleted本討論已刪除"
   end
 
 private
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+        redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
   def group_params
     params.require(:group).permit(:title, :description)
   end
